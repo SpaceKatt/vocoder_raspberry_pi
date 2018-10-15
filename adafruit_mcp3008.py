f57a2ddaa2a5ebd4a5fab1c317b75e5522bf55f4
Thomas Kercheval <spacekattpoispin@gmail.com>
2018-10-15 02:06:12 +0000

    Ensure proper cleanup of resources

--- adafruit_mcp3008.py
+++ adafruit_mcp3008.py
@@ -7,8 +7,10 @@ import time
 import os
 import RPi.GPIO as GPIO
 
+# import ctcsound
+
 GPIO.setmode(GPIO.BCM)
-DEBUG = 1
+DEBUG = 0
 
 # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
@@ -22,6 +24,7 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         commandout = adcnum
         commandout |= 0x18  # start bit + single-ended bit
         commandout <<= 3    # we only need to send 5 bits here
+
         for i in range(5):
                 if (commandout & 0x80):
                         GPIO.output(mosipin, True)
@@ -41,7 +44,7 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
                         adcout |= 0x1
 
         GPIO.output(cspin, True)
-        
+
         adcout >>= 1       # first bit is 'null' so drop it
         return adcout
 
@@ -66,6 +69,7 @@ tolerance = 5       # to keep from being jittery we'll only change
                     # volume when the pot has moved more than 5 'counts'
 
 while True:
+    try:
         # we'll assume that the pot didn't move
         trim_pot_changed = False
 
@@ -75,31 +79,25 @@ while True:
         pot_adjust = abs(trim_pot - last_read)
 
         if DEBUG:
-                print "trim_pot:", trim_pot
-                print "pot_adjust:", pot_adjust
-                print "last_read", last_read
+                print("trim_pot:", trim_pot)
+                print("pot_adjust:", pot_adjust)
+                print("last_read", last_read)
 
         if ( pot_adjust > tolerance ):
                trim_pot_changed = True
 
         if DEBUG:
-                print "trim_pot_changed", trim_pot_changed
+                print("trim_pot_changed", trim_pot_changed)
 
-        if ( trim_pot_changed ):
-                set_volume = trim_pot / 10.24           # convert 10bit adc0 (0-1024) trim pot read into 0-100 volume level
-                set_volume = round(set_volume)          # round out decimal value
-                set_volume = int(set_volume)            # cast volume as integer
+        time_delta = 0.05
 
-                print 'Volume = {volume}%' .format(volume = set_volume)
-                set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' .format(volume = set_volume)
-                os.system(set_vol_cmd)  # set volume
-
-                if DEBUG:
-                        print "set_volume", set_volume
-                        print "tri_pot_changed", set_volume
-
-                # save the potentiometer reading for the next loop
-                last_read = trim_pot
+        weight = 3.1459 * time_delta
+        last_read += weight * (trim_pot - last_read)
 
         # hang out and do nothing for a half second
-        time.sleep(0.5)
+        time.sleep(time_delta)
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt! Stop the presses!")
+        print("Cleanup!")
+        GPIO.cleanup()
+        exit()
